f74df4cae1622ed24efbd9e89abebadda856bd4e
Allan Debastiani <allandeba@yahoo.com.br>
2024-02-13 18:20:22 -0300

    add project from Desafio-02

---
+++ Imc/Models/Enums/EGenero.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Imc.Models.Enums;
+
+public enum EGenero
+{
+    [Display(Name = "Não informado")]
+    NaoInformado,
+    Masculino,
+    Feminino
+}
