8053414e621fc32320808b3e8e77481a1dd3c8b5
Allan Debastiani <allandeba@yahoo.com.br>
2024-02-17 22:44:48 -0300

    feat ImcCalculatorService

--- Imc/Models/ImcCalculator.cs
+++ Imc/Models/ImcCalculator.cs
@@ -47,46 +47,4 @@ public class ImcCalculator
             return diff.ToString("dd/MM/yyyy");
         }
     }
-
-    public ImcResult Calculate()
-    {
-        if (!(Height.HasValue && Weight.HasValue))
-            throw new Exception("Passou na validação do modelo e não deveria ter passado!");
-
-        double HeightInMeters = Height.Value / 100.0;
-        double result = Weight.Value / (HeightInMeters * HeightInMeters);
-        return GetImcResult(result);
-    }
-
-    private ImcResult GetImcResult(double imc)
-    {
-        const string Warning = "Cuidado você está {0}, dê uma atenção para sua saúde";
-        const string Congrats = "Você está {0}, continue mantendo este estilo!";
-
-        var ImcResult = new ImcResult();
-        if (imc < 18.5) {
-            ImcResult.Title = EImcStatus.Magreza.ToString();
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
-        } else if (imc >= 18.5 && imc < 25) {
-            ImcResult.Title = EImcStatus.Normal.ToString();
-            ImcResult.Body = string.Format(Congrats, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Bom;
-        } else if (imc >= 25 && imc < 30) {
-            ImcResult.Title = EImcStatus.Sobrepeso.ToString();
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
-        } else if (imc >= 30 && imc < 40) {
-            ImcResult.Title = EImcStatus.Obesidade.ToString();
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
-        } else {
-            // Todo: Fazer o Display name Extension para enumeradores
-            ImcResult.Title = "Obesidade Grave";
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
-        }
-
-        return ImcResult;
-    }
 }
