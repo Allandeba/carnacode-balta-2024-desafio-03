2839bb0218c4c1c95294c836bff5fb3f5ecf974f
Allan Debastiani <allandeba@yahoo.com.br>
2024-02-18 22:14:12 -0300

    style coding

--- Imc/Services/ImcCalculatorService.cs
+++ Imc/Services/ImcCalculatorService.cs
@@ -27,30 +27,30 @@ public class ImcCalculatorService : IImcCalculatorService
         const string Warning = "Cuidado você está {0}, dê uma atenção para sua saúde";
         const string Congrats = "Você está {0}, continue mantendo este estilo!";
 
-        var ImcResult = new ImcResult();
+        var imcResult = new ImcResult();
         if (imc < 18.5) {
-            ImcResult.Title = EImcStatus.Magreza.ToString();
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
+            imcResult.Title = EImcStatus.Magreza.ToString();
+            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Status = EStatusImc.Ruim;
         } else if (imc >= 18.5 && imc < 25) {
-            ImcResult.Title = EImcStatus.Normal.ToString();
-            ImcResult.Body = string.Format(Congrats, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Bom;
+            imcResult.Title = EImcStatus.Normal.ToString();
+            imcResult.Body = string.Format(Congrats, imcResult.Title);
+            imcResult.Status = EStatusImc.Bom;
         } else if (imc >= 25 && imc < 30) {
-            ImcResult.Title = EImcStatus.Sobrepeso.ToString();
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
+            imcResult.Title = EImcStatus.Sobrepeso.ToString();
+            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Status = EStatusImc.Ruim;
         } else if (imc >= 30 && imc < 40) {
-            ImcResult.Title = EImcStatus.Obesidade.ToString();
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
+            imcResult.Title = EImcStatus.Obesidade.ToString();
+            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Status = EStatusImc.Ruim;
         } else {
             // Todo: Fazer o Display name Extension para enumeradores
-            ImcResult.Title = "Obesidade Grave";
-            ImcResult.Body = string.Format(Warning, ImcResult.Title);
-            ImcResult.Status = EStatusImc.Ruim;
+            imcResult.Title = "Obesidade Grave";
+            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Status = EStatusImc.Ruim;
         }
 
-        return ImcResult;
+        return imcResult;
     }
 }
