41fd047a7453432233b86b2f1a0255820bc8d8ef
Allan Debastiani <allandeba@yahoo.com.br>
2024-02-22 20:21:05 -0300

    feat HIstory.OrderBy

--- Imc/Data/Repository.cs
+++ Imc/Data/Repository.cs
@@ -22,10 +22,10 @@ public class LocalStorageRepository : ILocalStorageRepository
     public async Task<List<ImcCalculator>?> GetItemListAsync()
     {
         var modelList = await _localStorage.GetItemAsync<List<ImcCalculator>>(StorageConstants.Key);
-        if (modelList is not null)
-            modelList.Reverse();
-
-        return modelList;
+        if (modelList is null) return modelList;
+        
+        var ordenedList = modelList.OrderByDescending(x => x.CreatedDateTime).ToList();
+        return ordenedList;
     }
 
     public async Task SetItemAsync(IList<ImcCalculator> value) => await _localStorage.SetItemAsync(StorageConstants.Key, value);
