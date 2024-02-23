bc16ec82271e005a543183203c9508d1a76bd962
Allan Debastiani <allandeba@yahoo.com.br>
2024-02-22 23:36:51 -0300

    feat imc number to message

--- Imc/Services/ImcCalculatorService.cs
+++ Imc/Services/ImcCalculatorService.cs
@@ -19,29 +19,30 @@ public class ImcCalculatorService : IImcCalculatorService
 
     private ImcResult GetImcResult(double imc)
     {
-        const string Warning = "Cuidado você está {0}, dê uma atenção para sua saúde";
-        const string Congrats = "Você está {0}, continue mantendo este estilo!";
+        const string Warning = "Cuidado seu IMC é de: {0}, {1}, dê uma atenção para sua saúde.";
+        const string Congrats = "Seu IMC é de: {0}, {1}, continue mantendo este estilo!";
 
         var imcResult = new ImcResult();
+        imc = Math.Round(imc, 2);
         if (imc < 18.5) {
             imcResult.Title = EImcStatus.Magreza.ToString();
-            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Body = string.Format(Warning, imc, imcResult.Title);
             imcResult.Status = EStatusImc.Ruim;
         } else if (imc >= 18.5 && imc < 25) {
             imcResult.Title = EImcStatus.Normal.ToString();
-            imcResult.Body = string.Format(Congrats, imcResult.Title);
+            imcResult.Body = string.Format(Congrats, imc, imcResult.Title);
             imcResult.Status = EStatusImc.Bom;
         } else if (imc >= 25 && imc < 30) {
             imcResult.Title = EImcStatus.Sobrepeso.ToString();
-            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Body = string.Format(Warning, imc, imcResult.Title);
             imcResult.Status = EStatusImc.Ruim;
         } else if (imc >= 30 && imc < 40) {
             imcResult.Title = EImcStatus.Obesidade.ToString();
-            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Body = string.Format(Warning, imc, imcResult.Title);
             imcResult.Status = EStatusImc.Ruim;
         } else {
             imcResult.Title = EImcStatus.ObesidadeGrave.GetDisplayName();
-            imcResult.Body = string.Format(Warning, imcResult.Title);
+            imcResult.Body = string.Format(Warning, imc, imcResult.Title);
             imcResult.Status = EStatusImc.Ruim;
         }
 
